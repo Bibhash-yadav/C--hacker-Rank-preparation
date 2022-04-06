caed016f99f443fa09766e33d79a880cf607ce45
techxtor <techxtor472@gmail.com>
2022-04-06 20:17:16 +0545

    code

---
+++ Array.cpp
@@ -0,0 +1,26 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int array[n];
+
+    for(int i = 0; i < n; i++)
+   {
+        cin >> array[i];
+    }
+
+    for(int i = n - 1; i >= 0; i--)
+   {
+        cin >> array[i];
+        cout << array[i] << " ";
+    }
+    return 0;
+}
---
+++ pointer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+void update(int a,int b) {
+    // Complete this function    
+}
+
+int main() {
+    int a, b;
+cin>>a>>b;
+
+cout<<a+b<<endl;
+cout<<abs(a-b);
+
+    return 0;
+}
